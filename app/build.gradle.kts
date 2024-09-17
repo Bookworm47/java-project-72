65b2242cf18cb88e138adef3b885ee91e41fd842
Konstantin <samsonovkostya@gmail.com>
2024-09-17 16:21:28 +0300

    Third step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,21 +20,31 @@ application {
 }
 
 dependencies {
-    implementation("com.h2database:h2:2.2.224")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("org.apache.commons:commons-text:1.11.0")
-    implementation("gg.jte:jte:3.1.9")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("org.apache.commons:commons-collections4:4.4")
-    implementation("io.javalin:javalin:6.3.0")
-    implementation("io.javalin:javalin-bundle:6.3.0")
-    implementation("io.javalin:javalin-rendering:6.3.0")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
-
-    testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("gg.jte:jte:3.1.10")
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("org.thymeleaf:thymeleaf:3.1.1.RELEASE")
+    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.2.0")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-java8time:3.0.4.RELEASE")
+    implementation("org.webjars:bootstrap:5.2.3")
+    implementation("com.konghq:unirest-java:3.14.5")
+    implementation("org.jsoup:jsoup:1.16.1")
+    implementation("org.postgresql:postgresql:42.7.3")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.0")
+    testImplementation("io.ebean:ebean-test:13.15.0")
+    testImplementation("com.konghq:unirest-java:3.14.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,33 +1,41 @@
 package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.DirectoryCodeResolver;
+import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.dto.MainPage;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.sql.SQLException;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class App {
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
     }
-
-//    private static String readResourceFile(String fileName) throws IOException {
-//        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-//            return reader.lines().collect(Collectors.joining("\n"));
-//        }
-//    }
 
     public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
@@ -35,20 +43,45 @@ public class App {
         app.start(getPort());
     }
 
+    static String getBD() {
+        String bd = System.getenv().getOrDefault("JDBC_DATABASE_URL",
+                "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        return bd;
+    }
+
+    private static String getContent(InputStream is) throws IOException {
+        try (var reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+
+    private static InputStream getFile(String fileName) {
+        var classLoader = App.class.getClassLoader();
+        var inputStream = classLoader.getResourceAsStream(fileName);
+        return inputStream;
+    }
+
     public static Javalin getApp() throws IOException, SQLException {
-        // System.setProperty("h2.traceLevel", "TRACE_LEVEL_SYSTEM_OUT=4");
-
         var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        var dataBaseUrl = getBD();
+        if (dataBaseUrl == null || dataBaseUrl.equals("JDBC_DATABASE_URL")) {
+            hikariConfig.setJdbcUrl(dataBaseUrl);
+        } else {
+            hikariConfig.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
+            hikariConfig.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
+            hikariConfig.setJdbcUrl(dataBaseUrl);
+        }
 
-//        var dataSource = new HikariDataSource(hikariConfig);
-//        var sql = readResourceFile("schema.sql");
+        var dataSource = new HikariDataSource(hikariConfig);
 
-//        log.info(sql);
-//        try (var connection = dataSource.getConnection();
-//             var statement = connection.createStatement()) {
-//            statement.execute(sql);
-//        }
+        var sql = getContent(getFile("schema.sql"));
+        log.info(sql);
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+
+        BaseRepository.dataSource = dataSource;
 
         Path templateRoot = Path.of("src/main/resources/templates");
         TemplateEngine templateEngine = TemplateEngine.create(new DirectoryCodeResolver(templateRoot),
